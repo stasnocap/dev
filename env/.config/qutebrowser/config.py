c0a9aa2d020fe742375142b15e0447f086976b3d
stasnocap <stasnocap@gmail.com>
2025-07-19 22:34:58 +0300

    automated dev commit

--- env/.config/qutebrowser/config.py
+++ env/.config/qutebrowser/config.py
@@ -37,9 +37,9 @@ config.bind('cc', 'yank')
 config.bind(';C', 'hint links yank-primary')
 config.bind(';c', 'hint links yank')
 
-config.bind('w', 'hint links spawn mpv --force-window=immediate --input-ipc-server=/tmp/mpv-socket --ytdl-format="bestvideo[height<=?2160]+bestaudio/best" {hint-url}')
+config.bind('w', 'hint links spawn mpv --force-window=immediate --input-ipc-server=/tmp/mpv-socket --ytdl-format="bestvideo+bestaudio/best" --ytdl-raw-options="cookies-from-browser=brave" {hint-url}')
 config.bind('a', 'hint links spawn bash -c "echo \'loadfile {hint-url} append\' | socat - /tmp/mpv-socket"')
-config.bind('W', 'hint links spawn mpv --force-window=immediate --input-ipc-server=/tmp/mpv-socket --ytdl-raw-options="yes-playlist=,format=bestvideo[height<=?2160]+bestaudio/best" {hint-url}')
+config.bind('W', 'hint links spawn mpv --force-window=immediate --input-ipc-server=/tmp/mpv-socket --ytdl-raw-options="yes-playlist=,format=bestvideo+bestaudio/best" --ytdl-raw-options="cookies-from-browser=brave" {hint-url}')
 
 config.unbind('<Ctrl-V>')
 config.unbind('v')
